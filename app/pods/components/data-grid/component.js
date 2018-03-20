4fdeae1369b026645ec864cec33e300a2476d5cb
Theepan Kanthavel <jatheepan2008@yahoo.com>
2018-03-20 07:09:58 +0000

    Added header styles

--- app/pods/components/data-grid/component.js
+++ app/pods/components/data-grid/component.js
@@ -1,6 +1,12 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
   data: null,
-  title: null
+  columns: Ember.computed( 'data', function(){
+    console.log('sdfdsf')
+    const data = this.get( 'data.firstObject' );
+    console.log( Object.keys( data ) )
+    return Object.keys( data );
+  })
 });
