f5fed32ccaef41762421392bae79479651485198
Theepan Kanthavel <jatheepan2008@yahoo.com>
2018-03-21 09:36:36 +0000

    Product show page

--- app/pods/products/show/controller.js
+++ app/pods/products/show/controller.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   publisher: Ember.computed.alias('model.publisher'),
   product: Ember.computed.alias('model.product'),
-  productData: Ember.computed.alias('product.data')
+  productData: Ember.computed.alias('product.data'),
+  title: Ember.computed.alias('publisher.title'),
+
+  actions: {
+    click() {
+      console.log( 'clicked' );
+    }
+  }
 });
